6f983995943be0c40e5c5a996eab577d58d05e9e
jolveyra <jolveyra@gmail.com>
2023-07-28 08:39:51 -0300

    lee todos los archivos excel del directorio

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
+import fs from "fs";
+import path from "path";
 import excel from "exceljs";
 
+async function leerTodosLosArchivosExcelEnDirectorio(directoryPath) {
+  try {
+    const archivos = fs.readdirSync(directoryPath);
+
+    for (const archivo of archivos) {
+      if (path.extname(archivo) === ".xlsx") {
+        const filePath = path.join(directoryPath, archivo);
+        console.log(`Leyendo datos del archivo: ${filePath}`);
+        const datos = await leerDatosExcel(filePath);
+        if (datos) {
+          console.log("Datos leídos del archivo Excel:");
+          console.table(datos);
+        }
+      }
+    }
+  } catch (error) {
+    console.error("Error al leer los archivos Excel:", error.message);
+  }
+}
+
 // Función para leer los datos del archivo Excel
-async function leerDatosExcel() {
+async function leerDatosExcel(filePath) {
   try {
     const workbook = new excel.Workbook();
-    await workbook.xlsx.readFile("./BD.xlsx");
+    await workbook.xlsx.readFile(filePath);
     const worksheet = workbook.getWorksheet("Sheet1");
     const datos = [];
 
@@ -20,8 +42,9 @@ async function leerDatosExcel() {
   }
 }
 
-// Ejecutar la función para leer los datos del archivo Excel
-leerDatosExcel().then((datos) => {
+// Llama a la función para leer todos los archivos Excel en el directorio
+const directorio = "./";
+leerTodosLosArchivosExcelEnDirectorio(directorio).then((datos) => {
   if (datos) {
     console.log("Datos leídos del archivo Excel:");
     console.log(datos);
